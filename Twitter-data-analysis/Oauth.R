a1e855ea8ee66841721d69bda49e4eb08e7117e5
sravya pidugu <sravyapidugu@sravyas-mbp.home>
2018-09-24 22:21:59 -0400

    final commit

---
+++ Twitter-data-analysis/Oauth.R
@@ -0,0 +1,8 @@
+require(httr)
+require(twitteR)
+require(RCurl)
+api_key<-""
+api_secret<-""
+access_token<-""
+access_token_secret<-""
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
